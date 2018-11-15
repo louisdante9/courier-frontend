63e6ca8fc8232de2ebebe6931183557df1aeb8ed
Louis Nwamadi <louisdante9@gmail.com>
2018-11-15 05:33:50 +0100

    Updates

---
+++ precache-manifest.da6594721cd8b4c0660cf647be2a46a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f986893c8b82e490971e57b8f56f18c",
+    "url": "/courier/static/media/trakpak-9842.1f986893.png"
+  },
+  {
+    "revision": "a4dd231594363badaaa43d7fde58fd3d",
+    "url": "/courier/static/media/cloud-screenshot.a4dd2315.png"
+  },
+  {
+    "revision": "303d3ee9f1b434f88dbd14e5182479c8",
+    "url": "/courier/static/media/citysprint.303d3ee9.png"
+  },
+  {
+    "revision": "b07dd1b93b120197697f",
+    "url": "/courier/static/js/runtime~main.b07dd1b9.js"
+  },
+  {
+    "revision": "7b0c3784d784e9fdec70",
+    "url": "/courier/static/js/main.7b0c3784.chunk.js"
+  },
+  {
+    "revision": "c367277f1f715c642053",
+    "url": "/courier/static/js/1.c367277f.chunk.js"
+  },
+  {
+    "revision": "7b0c3784d784e9fdec70",
+    "url": "/courier/static/css/main.c7170273.chunk.css"
+  },
+  {
+    "revision": "51dbeea08d32c58f449e5524166845c2",
+    "url": "/courier/index.html"
+  }
+];
